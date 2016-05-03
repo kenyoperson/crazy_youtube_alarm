a703404fb9cb88565c0cad84a079f055ddff4bcf
Kenneth Dabis <kdabis2017@room-117.local>
2016-05-03 14:16:35 -0700

    initial commit, add uiview

--- youwakeup/ViewController.swift
+++ youwakeup/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = "https://www.youtube.com/embed/Rg6GLVUnnpM?rel=0&autoplay=1"
+        
+        videoView.allowsInlineMediaPlayback = true
+        
+        videoView.loadHTMLString("<iframe width=\"560\" height=\"315\" src=\"\(url)/&autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openVideo(sender: AnyObject) {
+        if let url = NSURL(string: "https://youtu.be/OmBge5GWZ5Y?t=138") {
+            let request = NSURLRequest(URL: url)
+            videoView.loadRequest(request)
+        }
+    }
 
 }
 
