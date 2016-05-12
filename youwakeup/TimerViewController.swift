464a39541eff6f1a462882dee8a2039362ee049d
Kenneth Dabis <kdabis2017@room-117.local>
2016-05-12 12:23:17 -0700

    made working alarm! fixed picker view

--- youwakeup/TimerViewController.swift
+++ youwakeup/TimerViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class TimerViewController: UIViewController {
+    
+    
+    @IBOutlet weak var timerLabel: UILabel!
 
+    @IBOutlet weak var timerPicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        timerPicker.addTarget(self, action: Selector("timerPickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    
         // Do any additional setup after loading the view.
     }
 
@@ -31,5 +38,28 @@ class TimerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func timerPickerChanged(timerPicker:UIDatePicker) {
+        var timerFormat = NSDateFormatter()
+        
+        timerFormat.dateStyle = NSDateFormatterStyle.NoStyle
+        timerFormat.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        var strDate = timerFormat.stringFromDate(timerPicker.date)
+        timerLabel.text = strDate
+        
+    
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        let date = NSDate().dateByAddingTimeInterval(1)
+        let timerDate = NSTimer(fireDate: timerPicker.date, interval: 0, target: self, selector: "timeLabel", userInfo: nil, repeats: false)
+        NSRunLoop.mainRunLoop().addTimer(timerDate, forMode: NSRunLoopCommonModes)
+    }
+    
+    func timeLabel() {
+        timerLabel.text = "ye"
+    }
+    
+    
 }
