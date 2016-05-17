be374e09ae0500db88d108b11576a9921646caa9
Rodrigo Palacios-Guzman <rpalaciosguzman2016@room-208-9.local>
2016-05-17 14:26:06 -0700

    big change to storyboard. made link input bar work with videoView

--- youwakeup/ViewController.swift
+++ youwakeup/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var testLabel: UILabel!
     @IBOutlet weak var websiteUrlInput: UITextField!
@@ -18,8 +18,9 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
+        websiteUrlInput.delegate = self;
         
-    var videoLink = websiteUrlInput.text
+        var videoLink = websiteUrlInput.text
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,8 +31,6 @@ class ViewController: UIViewController {
         
         //videoView.loadHTMLString("<iframe width=\"560\" height=\"315\" src=\"\(url)/&autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil)
         
-        videoView.loadHTMLString("<iframe width=\"100\" height=\"450\" scrolling=\"no\" frameborder=\"no\" src=\"\(url)/auto_play=true\"></iframe>"
-            , baseURL: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,26 +38,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func openVideo(sender: AnyObject) {
-        if let url = NSURL(string: "https://www.google.com") {
-            let request = NSURLRequest(URL: url)
-            videoView.loadRequest(request)
-            
-        
-            //https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/237603952&amp&auto_play=true
-        }
-    }
+//    @IBAction func openVideo(sender: AnyObject) {
+//        if let url = NSURL(string: "https://www.google.com") {
+//            let request = NSURLRequest(URL: url)
+//            videoView.loadRequest(request)
+//            
+//        
+//            //https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/237603952&amp&auto_play=true
+//        }
+//    }
     
-    @IBAction func WebsiteUrlAction(sender: AnyObject) {
+//    @IBAction func WebsiteUrlAction(sender: AnyObject) {
+//        print("in websiteUrlAction")
+//        
+//        if let input = websiteUrlInput.text {
+//            print("in let input")
+//            websiteUrlInput.text = input
+//            
+//            if let url = NSURL(string: websiteUrlInput.text!){
+//                videoView.loadRequest(NSURLRequest(URL: url))
+//            }
+//        }
+//        
+//    }
+    
+    func textFieldShouldReturn(websiteUrlInput: UITextField) -> Bool {
+        print("in textFieldShould")
+        websiteUrlInput.resignFirstResponder()
         
-        if let input = websiteUrlInput.text {
-            websiteUrlInput.text = input
+        if let url = NSURL(string: websiteUrlInput.text!){
+            videoView.loadRequest(NSURLRequest(URL: url))
+            print("in let url")
         }
-
         
+        return true
     }
-
 }
+
+
+
 
 
 
